331ca7617a34c1afa7f77fe396fd07e97fcfcc82
Gabriel DUSHIME <108078955+GabrielDushime@users.noreply.github.com>
2024-03-11 17:33:17 +0200

    working on the Responsivness

--- Blogs.js
+++ Blogs.js
@@ -7,3 +7,16 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+/*Side Navigation*/
+function openNav()
+{
+    document.getElementById("mySidenav").style.width = "118px";
+}
+
+function closeNav() 
+{
+   document.getElementById("mySidenav").style.width = "0";
+}
+
+/*Copyright*/
+document.getElementById("current-year").textContent = new Date().getFullYear();
