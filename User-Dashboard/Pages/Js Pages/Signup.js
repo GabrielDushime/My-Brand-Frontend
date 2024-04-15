519de4fe72a29212c25f95298ab3ae06f4d4734f
Gabriel DUSHIME <108078955+GabrielDushime@users.noreply.github.com>
2024-04-15 10:41:06 +0200

    users integrations

--- User-Dashboard/Pages/Js Pages/Signup.js
+++ User-Dashboard/Pages/Js Pages/Signup.js
@@ -1,65 +1,39 @@
-/*Side Navigation*/
-function openNav()
-{
-    document.getElementById("mySidenav").style.width = "118px";
-}
-
-function closeNav() 
-{
-   document.getElementById("mySidenav").style.width = "0";
-}
-
-
-
-
-
-
-/*Form Validation*/
-
 function validateForm() {
     let password = document.getElementById("password").value;
-    const cpassword= document.getElementById("cpassword").value;
-    const fname= document.getElementById("fname").value;
-    const lname= document.getElementById("lname").value;
-    const email= document.getElementById("email").value;
-   
-    if (fname === "")
-    {
+    const cpassword = document.getElementById("cpassword").value;
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    const email = document.getElementById("email").value;
+
+    console.log("Password:", password);
+    console.log("Confirm Password:", cpassword);
+
+    if (fname === "") {
         document.getElementById("fname-error").style.display = "inline";
-       return false;
+        return false;
     }
-    if (lname === "")
-    {
+    if (lname === "") {
         document.getElementById("lname-error").style.display = "inline";
-       return false;
+        return false;
     }
-     if(email  === "" || !email.includes('@'))
-    {
+    if (email === "" || !email.includes('@')) {
         document.getElementById("email-error").style.display = "inline";
-       return false;
-     
-     }
-    if (password === "")
-    {
+        return false;
+    }
+    if (password === "") {
         document.getElementById("password-error").style.display = "inline";
         return false;
     }
-  
-    
-    if (cpassword === "")
-    {
+
+    if (cpassword === "") {
         document.getElementById("cpassword-error").style.display = "inline";
         return false;
     }
-    
-    if (cpassword !== password)
-    {
+
+    if (cpassword !== password) {
         document.getElementById("matchpasscode-error").style.display = "inline";
-       return false;
+        return false;
     }
-  
-    {
-        return true;
-    }
-   
+
+    return true; 
 }
